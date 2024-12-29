d1a4ca0661d77a53b0624282eb6660bba34da74e
awithliee <alijr282006@gmail.com>
2024-12-29 17:17:17 +0700

    100%

--- portofolio/style.js
+++ portofolio/style.js
@@ -1,6 +1,7 @@
-const menuToggle = document.querySelector('.menu-toggle input');
-const nav =document.querySelector('nav ul');
+const toogle = document.querySelector('.toggle input');
+const nav = document.querySelector('nav ul');
 
-menuToggle.addEventListener('click', function() {
-    nav.classList.toggle('slide');
-}); 
+
+toogle.addEventListener('click',function(){
+nav.classList.toggle('slide');
+});
